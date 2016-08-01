836caa2ab60fb833516c1bc8577b2dae12898dd5
garageposte3 <hugo.david@centrale-marseille.fr>
2016-08-01 19:36:18 +0200

    Creation

---
+++ controllers/main.controller.js
@@ -0,0 +1,12 @@
+var FirebaseService = require('../services/firebase.service');
+
+class MainCtrl {
+	constructor() {
+	}
+
+	help1(req, res) {
+		
+	}
+}
+
+module.exports = new MainCtrl();
---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var MainCtrl = require('./controllers/main.controller');
+//var config = require('./config');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+	next();
+});
+
+app.get('/', (req, res) => {
+	res.status(200).send('Hello world !');
+});
+
+// helper 1
+app.get('/help1', (req, res) => {
+	MainCtrl.help1(req, res);
+});
+
+app.listen(3001, () => {
+	console.log('App listening on port 3001');
+});
